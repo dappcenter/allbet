01b1698cdd5fe1f881bba14647307f2c7cc90f4a
shanks <zengzuozuo@163.com>
2018-12-11 18:42:50 +0800

    设备检查优化

--- src/util/extend_methods.js
+++ src/util/extend_methods.js
@@ -113,10 +113,10 @@ const getLanguage = () => {
 }
 
 const IsPC = () => {
-    var userAgentInfo = navigator.userAgent;
+	var userAgentInfo = navigator.userAgent;
     var Agents = ["Android", "iPhone",
                 "SymbianOS", "Windows Phone",
-                "iPad", "iPod"];
+                "iPad", "iPod", "android"];
     var flag = true;
     for (var v = 0; v < Agents.length; v++) {
         if (userAgentInfo.indexOf(Agents[v]) > 0) {
