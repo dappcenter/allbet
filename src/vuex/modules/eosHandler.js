117dd8416b6d554db2cb3b7d099c8378b2a66c4e
shanks <zengzuozuo@163.com>
2019-01-03 11:24:06 +0800

    eos支持准备

--- src/vuex/modules/eosHandler.js
+++ src/vuex/modules/eosHandler.js
@@ -10,26 +10,24 @@ const mutations = {
 
 const actions = {
     registerEOS({commit, rootState}) {
-        console.log(eossdkutil)
         if (eossdkutil) {
             window.eossdkutil = eossdkutil;
-            
             eossdkutil.setScatterNetworkTest();//按需选择测试或正式网络
-            // eossdkutil.setScatterNetworkMain();
-            // eossdkutil.init().then(function () {
-            //     var env = eossdkutil.getEnv();
-            //     if (env == "tp") {
-            //     //当前环境是tp
-            //     } else if (env == "scatter") {
-            //     //当前环境是scatter
-            //         console.log("scatter")
-            //         console.log(eossdkutil.getScatterIdentity())
-            //         eossdkutil.login().then((identity) => {
-            //             console.log(identity)
-            //         });
-            //     } else {
-            //     }
-            // });
+            eossdkutil.setScatterNetworkMain();
+            eossdkutil.init().then(function () {
+                var env = eossdkutil.getEnv();
+                if (env == "tp") {
+                    //当前环境是tp
+                } else if (env == "scatter") {
+                    //当前环境是scatter
+                    console.log("scatter")
+                    console.log(eossdkutil.getScatterIdentity())
+                    eossdkutil.login().then((identity) => {
+                        console.log(identity)
+                    });
+                } else {
+                }
+            });
         }
     }
 }
