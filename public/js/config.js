5504e7f6959cbd33d0af46409842f4f1a0b8eda1
zengzuozuo <zengzuozuo@163.com>
2019-01-20 18:05:44 +0800

    暂存

--- public/js/config.js
+++ public/js/config.js
@@ -35,20 +35,20 @@ if(window.location.href.indexOf('www.allbet.io') >= 0) {
 
 }else {
     //本地环境
-    // window.SERVERPATH = "http://192.168.120.141:8080"
-    // // window.SERVERPATH = "http://47.91.247.93:7788"
-    // window.BANCORADDRESS = "0xd06b02f187c137f59f9351c62a421dae59739d37"
-    // window.ROLLERADDRESS = "0x9fb54a29924ecca77e8f2c6c7e762e9e18e56b04"
-    // window.ETHPLEDGEADDRESS = "0xc4476e362084b1b28bf38695ca05f88a64cce8db"  //抵押合约
-    // window.ETHABTOKEN = "0x1f1a8108671c893AD3382166Edd427E487be3266"
+    window.SERVERPATH = "http://192.168.120.141:8080"
+    // window.SERVERPATH = "http://47.91.247.93:7788"
+    window.BANCORADDRESS = "0xd06b02f187c137f59f9351c62a421dae59739d37"
+    window.ROLLERADDRESS = "0x9fb54a29924ecca77e8f2c6c7e762e9e18e56b04"
+    window.ETHPLEDGEADDRESS = "0xc4476e362084b1b28bf38695ca05f88a64cce8db"  //抵押合约
+    window.ETHABTOKEN = "0x1f1a8108671c893AD3382166Edd427E487be3266"
 
-    // window.TRONROLLARADDRESS = "TST9xJjC5SRSaF9geUrAwjhRVCFa9VDrkj"
-    // window.TRONFUNDRAIYADDRESS = "TJdfA7GzgV8MtBBAxKJ5KtCpC9iH5kVxtN"
-    // window.TRONFUNDRAIYADDRESS2 = "TGCz7hLkvBamcSh7TipdFtBaSM2DXeY5aT"  //合约转账
-    // window.TRONPLEDGEADDRESS = "TQTeA7u1WWYghEJPC8D5jH5qX9aSePAcpK"  //抵押合约
-    // window.TRONABTOKEN = "TAgToUuR4ZPiWWWsdGtR2NMJfbs7Hc6cqG"
-    // window.TRONPOKERADDRESS = "TFcjC9q3w2KUGpf7dCqkLmNUKQz2P8a4Bb"  //poker游戏合约
+    window.TRONROLLARADDRESS = "TST9xJjC5SRSaF9geUrAwjhRVCFa9VDrkj"
+    window.TRONFUNDRAIYADDRESS = "TJdfA7GzgV8MtBBAxKJ5KtCpC9iH5kVxtN"
+    window.TRONFUNDRAIYADDRESS2 = "TGCz7hLkvBamcSh7TipdFtBaSM2DXeY5aT"  //合约转账
+    window.TRONPLEDGEADDRESS = "TQTeA7u1WWYghEJPC8D5jH5qX9aSePAcpK"  //抵押合约
+    window.TRONABTOKEN = "TAgToUuR4ZPiWWWsdGtR2NMJfbs7Hc6cqG"
+    window.TRONPOKERADDRESS = "TFcjC9q3w2KUGpf7dCqkLmNUKQz2P8a4Bb"  //poker游戏合约
 
-    // window.NETWORK = 4
+    window.NETWORK = 4
     
 }
